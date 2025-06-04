4ab70bce9c4929f9c6502fb00d413fee8504fc2a
Isaque Dantas <isaque.dantas@logap.com.br>
2025-06-04 15:29:22 -0300

    add test for answer checking

--- src/models.rs
+++ src/models.rs
@@ -24,6 +24,19 @@ impl Equation {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_is_answer_correct() {
+        let equation = Equation {id: 0, content: String::new(), answer: 10.3221346};
+        let correct_answer: f32 = 10.32;
+        
+        assert!(equation.is_answer_correct(correct_answer));
+    }
+}
+
 #[derive(Selectable, Insertable)]
 #[diesel(table_name = equations)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
